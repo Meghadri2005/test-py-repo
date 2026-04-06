556b94dce9063f40ef1aeb203de15b61d3d11919
Meghadri Pal <palmeghadri2005@gmail.com>
2026-04-06 15:52:35 +0530

    update

--- djangoproject/demo/DEMO/products/admin.py
+++ djangoproject/demo/DEMO/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Review, WishlistItem, ProductTracking
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(WishlistItem)
+admin.site.register(ProductTracking)
